6406b45feb3939691349115be0858d856fc6c1d4
t-rakuyama <t.rakuyama929@gmail.com>
2024-06-24 23:39:54 +0900

    feat: Taskの基本的なDB処理を作成

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Models\Task;
+
+class TaskRepository
+{
+    public function createTask(string $title)
+    {
+        Task::insert(['title' => $title]);
+    }
+
+    public function findAll()
+    {
+        return Task::all();
+    }
+
+    public function find(int $id)
+    {
+        return Task::find($id);
+    }
+
+    public function delete(int $id)
+    {
+        Task::destroy($id);
+    }
+
+    public function update(int $id, ?string $title, ?string $description, ?int $status, ?int $point)
+    {
+        $task = Task::find($id);
+        $task->title = $title;
+        $task->description = $description;
+        $task->status = $status;
+        $task->point = $point;
+        $task->save();
+    }
+}

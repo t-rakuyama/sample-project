423b876506e8e302508da56093070dceb45fa9b1
t-rakuyama <t.rakuyama929@gmail.com>
2024-06-29 23:37:35 +0900

    feat: APIとの繋ぎ込み

--- resources/ts/Pages/Welcome.tsx
+++ resources/ts/Pages/Welcome.tsx
@@ -1,18 +1,12 @@
+import axios from "axios"
 import { List } from "../Components/List"
 import { Button } from "../Components/Button"
 import { Header } from "../Components/Header/Header"
 import { Link, Head } from "@inertiajs/react"
-import React from "react"
+import React, { useRef, useState } from "react"
 import { ListItem } from "../Components/List/ListItem"
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
-  const handleImageError = () => {
-    document.getElementById("screenshot-container")?.classList.add("!hidden")
-    document.getElementById("docs-card")?.classList.add("!row-span-1")
-    document.getElementById("docs-card-content")?.classList.add("!flex-row")
-    document.getElementById("background")?.classList.add("!hidden")
-  }
-
   const taskList = [
     {
       id: 1,
@@ -37,6 +31,37 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
     },
   ]
 
+  const displayTextArea = () => {
+    let inputDom = document.getElementById("taskInput")
+    if (!inputDom) {
+      return
+    }
+    inputDom.style.display = "block"
+  }
+
+  const [data, setData] = useState({ title: "" })
+
+  const onSubmit = async () => {
+    await axios
+      .post("/api/task", data)
+      .then((res) => {
+        setData({ title: "" })
+        console.log(res)
+        location.href = "/"
+      })
+      .catch((e) => {
+        console.log(e.response)
+      })
+  }
+
+  const onChange = () => {
+    let taskForm = document.forms["task"]
+
+    setData({
+      title: taskForm.elements.title.value,
+    })
+  }
+
   return (
     <>
       <Head title="Welcome" />
@@ -45,7 +70,19 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
         <div className="relative min-h-screen flex flex-col items-center selection:bg-[#FF2D20] selection:text-white">
           <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
             <main className="mt-6">
-              <Button className={""} text={"add"} />
+              <div className="flex">
+                <Button className={""} text={"add"} onClick={displayTextArea} />
+                <form name="task" onSubmit={onSubmit}>
+                  <input
+                    id="taskInput"
+                    name="title"
+                    className="flex items-start space-x-6 text-black"
+                    type="text"
+                    style={{ display: "none" }}
+                    onChange={onChange}
+                  />
+                </form>
+              </div>
               <List>
                 {taskList.map((task, index) => (
                   <ListItem task={task} key={index} />

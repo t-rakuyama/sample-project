ab41990ed6a597b7a4c5716c654bd454850d3a1b
t-rakuyama <t.rakuyama929@gmail.com>
2024-06-29 23:36:13 +0900

    fix: ButtonコンポーネントでonClickを受け取れるよう修正

--- resources/ts/Components/Button/Button.tsx
+++ resources/ts/Components/Button/Button.tsx
@@ -3,13 +3,15 @@ import React from "react"
 type Props = {
   className: string
   text: string
+  onClick?: () => void
 }
 
-export const Button = ({ text }: Props) => {
+export const Button = ({ text, onClick }: Props) => {
   return (
     <button
       type="button"
       className="bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-transparent rounded"
+      onClick={onClick}
     >
       {text}
     </button>

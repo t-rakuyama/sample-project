bc6e9c8892c742c988eda586db952e76b0c7bc25
t-rakuyama <t.rakuyama929@gmail.com>
2024-06-29 23:31:07 +0900

    feat: Task作成APIにくるRequestを作成

---
+++ app/Http/Requests/TaskCreateRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskCreateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return ['title' => ['required', 'string', 'max:255']];
+    }
+}

f6cb06014bc65a930ab0eebc03266c1293e7e775
t-rakuyama <t.rakuyama929@gmail.com>
2024-06-30 14:14:23 +0900

    feat: Taskを取得するAPIのエンドポイントを作成

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskDetailController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -8,4 +9,5 @@ use Inertia\Inertia;
 Route::middleware('api')->group(function () {
     Route::post('/task', [TaskController::class, 'create']);
     Route::get('/task', [TaskController::class, 'index']);
+    Route::get("/task/{id}", [TaskDetailController::class, 'index']);
 });

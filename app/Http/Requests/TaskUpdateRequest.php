896c363e56799eb3e6b08aa2a3e6d35ec9c37db7
t-rakuyama <t.rakuyama929@gmail.com>
2024-08-04 13:43:45 +0900

    feat: タスク更新用のリクエストを作成

---
+++ app/Http/Requests/TaskUpdateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => ['required', 'int'],
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['string'],
+            'status' => ['string']
+        ];
+    }
+}

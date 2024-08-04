9f55709d969616e77bd30b7a4089dadbe18c740a
t-rakuyama <t.rakuyama929@gmail.com>
2024-08-04 16:02:57 +0900

    fixup! feat: TextAreaコンポーネントを作成

--- resources/ts/Components/TextArea/TextArea.tsx
+++ resources/ts/Components/TextArea/TextArea.tsx
@@ -3,7 +3,7 @@ import React from "react"
 type Props = {
   name: string
   className: string
-  defaultValue: string
+  defaultValue?: string
 }
 
 export const TextArea = ({ name, className, defaultValue }: Props) => (

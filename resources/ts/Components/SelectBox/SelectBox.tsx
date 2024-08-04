3a35812bfb4f8d4d8e6bb127f1076b6c66fc0a24
t-rakuyama <t.rakuyama929@gmail.com>
2024-08-04 16:24:48 +0900

    fixup! feat: SelectBoxのコンポーネント作成

--- resources/ts/Components/SelectBox/SelectBox.tsx
+++ resources/ts/Components/SelectBox/SelectBox.tsx
@@ -2,13 +2,13 @@ import React from "react"
 
 type Props = {
   name: string
-  onChange: () => void
+  onChange: (any) => void
   className: string
   selectValue: string
   values: { value: string; text: string }[]
 }
 
-export const TextArea = ({ name, onChange, className, selectValue, values }: Props) => (
+export const SelectBox = ({ name, onChange, className, selectValue, values }: Props) => (
   <select name={name} onChange={onChange} className={className} value={selectValue}>
     {values.map((content) => (
       <option value={content.value}>{content.text}</option>

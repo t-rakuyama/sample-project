712fb6630d3aa66fdebf48513f2902a5fc5ff14e
t-rakuyama <t.rakuyama929@gmail.com>
2024-08-05 23:46:36 +0900

    fix: SelectBoxを修正

--- resources/ts/Components/SelectBox/SelectBox.tsx
+++ resources/ts/Components/SelectBox/SelectBox.tsx
@@ -4,12 +4,13 @@ type Props = {
   name: string
   onChange: (any) => void
   className: string
-  selectValue: string
+  selectValue?: string
   values: { value: string; text: string }[]
 }
 
 export const SelectBox = ({ name, onChange, className, selectValue, values }: Props) => (
   <select name={name} onChange={onChange} className={className} value={selectValue}>
+    {!selectValue && <option value="">選択してください</option>}
     {values.map((content) => (
       <option value={content.value} key={content.value}>
         {content.text}

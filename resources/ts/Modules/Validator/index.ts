55f9dc4e4b40772a3eef08282f93c48d2e5a0139
樂山哲也 <t_rakuyama@F220613noMacBook-Pro.local>
2024-08-08 22:31:43 +0900

    feat: number型か判定するバリデーションを作成

--- resources/ts/Modules/Validator/index.ts
+++ resources/ts/Modules/Validator/index.ts
@@ -1,1 +1,2 @@
 export * from './stringValidator'
+export * from './numberValidator'
---
+++ resources/ts/Modules/Validator/numberValidator.ts
@@ -0,0 +1,1 @@
+export const numberValidator = (value?: number ): value is number => typeof value === "number" && !isNaN(value)
